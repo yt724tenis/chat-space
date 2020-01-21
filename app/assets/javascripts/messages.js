d8a5588e8ec338b81ba409c14164511cf6f1e27b
yt724tenis <57790346+yt724tenis@users.noreply.github.com>
2020-01-21 18:30:49 +0900

    chat-space自動更新機能の実装

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,8 +1,20 @@
 $(function (){
   function buildHTML(message){
-    if ( message.image ) {
+    if (message.text && message.image){
       var html =
-        `<div class="chat-main__message-name">
+        `<div class="chat-main__message-name" data-message-id= "${message.id}">
+          ${message.user_name}
+          <p class="chat-main__message-list-date">
+            ${message.created_at}
+          </p>
+        </div>
+        <p class="chat-main__message-list-message">
+          ${message.text}
+        </p>`
+      return html;
+    } else if ( message.image ) {
+      var html =
+        `<div class="chat-main__message-name" data-message-id= "${message.id}">
           ${message.user_name}
           <p class="chat-main__message-list-date">
             ${message.created_at}
@@ -14,9 +26,9 @@ $(function (){
           </img>
         </p>`
       return html;
-    } else {
+    } else if(message.text){
       var html =
-        `<div class="chat-main__message-name">
+        `<div class="chat-main__message-name" data-message-id= "${message.id}">
           ${message.user_name}
           <p class="chat-main__message-list-date">
             ${message.created_at}
@@ -27,13 +39,12 @@ $(function (){
         </p>`
       return html;
     };
-  } 
+  }
+ 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this);
-    
+    var formData = new FormData(this);  
     var url = $(this).attr('action')
-    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
@@ -44,6 +55,8 @@ $(function (){
     })
     .done(function(data){
       var html = buildHTML(data);
+      $('.chat-main__message').append(html);
+      $('form')[0].reset();
       $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight });
     })
     .always(function(){
@@ -52,5 +65,36 @@ $(function (){
     .fail(function() {
         alert("メッセージが送信されませんでした");
     });
-  })
+  });
+
+    var reloadMessages = function() {
+      var last_message_id = $('.chat-main__message-name:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+
+        if (messages.length !== 0) {
+          var insertHTML = '';
+          $.each(messages, function(i, message){
+            insertHTML += buildHTML(message)
+          });
+          $('.chat-main__message').append(insertHTML);
+          $('.chat-main__message').animate({ scrollTop: $('.chat-main__message')[0].scrollHeight});
+          $('#new_message')[0].reset();
+          $("message-form__send-btn").prop("disabled", false);
+        }  
+      })  
+      .fail(function() {
+        console.log('error');
+      });
+    };
+
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+      clearInterval()
+  } 
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,5 +1,6 @@
-
+json.id @message.id
 json.user_name @message.user.name
 json.created_at @message.created_at.strftime("%Y年%m月%d日 %H時%M分")
 json.text @message.text
 json.image @message.image.url
+
