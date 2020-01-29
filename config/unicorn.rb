c64e94a1fccc6bceb96ca08ebcde06f6e4e54fb3
yt724tenis <57790346+yt724tenis@users.noreply.github.com>
2020-01-29 20:54:21 +0900

    chat-space自動デフロイ

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,13 +5,15 @@ app_path = File.expand_path('../../../', __FILE__)
 worker_processes 1
 
 #アプリケーションの設置されているディレクトリを指定
-working_directory app_path "#{app_path}/current"
 
+
+working_directory "#{app_path}/current"
+# binding.pry
+#ポート番号を指定
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
-#ポート番号を指定
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
